99256c21fc20dd709b3d33763d42b3d5515a1082
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2023-01-05 23:06:31 +0100

    ifallatt

--- src/components/player.js
+++ src/components/player.js
@@ -2,44 +2,45 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {Playerpictures} from "./playerpictures"
+import { Playerpictures } from "./playerpictures"
 
 export function Player() {
 
     const { id } = useParams()
     const [player, setPlayer] = useState([{}]);
-    let medals = []
+    const [medalj, setMedalj] = useState([{}]);
 
     const url = "http://enkopingrugby.local/wp-json/wp/v2/posts/" + id + "?acf_format=standard";
 
     console.log(id);
     useEffect(() => {
         axios
-          .get(url
-          )
-          .then(response => {
-            setPlayer(response.data.acf)
-          });
-      }, [id]);
+            .get(url
+            )
+            .then(response => {
+                setPlayer(response.data.acf)
+            });
+    }, []);
 
-      useEffect(() => {
 
-        if(player.medal_5.placement != Number){
-            console.log("hej")
-        }
-        console.log(player.medal_5.placement);
-        medals.push(player.medal_1);
-        console.log(medals);
-      }, [player]);
+    useEffect(() => {
 
-  return (
-    <div>
-        <img src={player.profile_picture}></img>
-        <h1>{player.player_name}</h1>
-        <p>{player.position}</p>
-        <Playerpictures></Playerpictures>
+        let allMedals = [];
+        allMedals.push(player.medal_1, player.medal_2, player.medal_3, player.medal_4, player.medal_5, player.medal_6, player.medal_7, player.medal_8, player.medal_9)
+        setMedalj(allMedals);
+
+    }, [player]);
+
+console.log(medalj)
+
+    return (
+        <div>
+            <img src={player.profile_picture}></img>
+            <h1>{player.player_name}</h1>
+            <p>{player.position}</p>
+            <Playerpictures></Playerpictures>
         </div>
-  );
-        
+    );
+
 
 }
