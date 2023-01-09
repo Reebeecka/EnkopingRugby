86629c285e82da8d6c96ab80c904b4b52f524844
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2023-01-09 16:13:45 +0100

    Route to homepage on logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,19 @@
 import { Link, Outlet } from "react-router-dom";
 import logo from "../logo.png";
+import { useNavigate } from 'react-router-dom';
 
 export const Layout = () => {
+
+    let navigate = useNavigate();
+
+    const routeChange = () => {
+        let path = `/`;
+        navigate(path);
+      }
+
     return(<>
     <header>
-        <img src={logo}></img>
+        <img onClick={routeChange}  src={logo}></img>
         <h1>Enköping Rugbyklubb Damer</h1>
       </header>
       <main>
