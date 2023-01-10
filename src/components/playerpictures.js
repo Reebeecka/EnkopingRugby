b57539c92415c3474c5b5ee1fd40460a1f3ac946
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2023-01-10 16:09:49 +0100

    Bildspel

--- src/components/playerpictures.js
+++ src/components/playerpictures.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
 import axios from "axios";
 import { useEffect, useState } from "react";
+
+import LightGallery from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+// If you want you can use SCSS instead of css
+import 'lightgallery/scss/lightgallery.scss';
+import 'lightgallery/scss/lg-zoom.scss';
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
 export function Playerpictures(props) {
 
     const [allImages, setAllImages] = useState([{}]);
 
     let imagesHTML = <></>
+    let testHTML = <></>
 
     const url = "http://enkopingrugby.local/wp-json/wp/v2/posts/" + props.id + "?acf_format=standard";
 
@@ -19,6 +35,26 @@ export function Playerpictures(props) {
             });
     }, []);
 
+    if (allImages != false) {
+        if (allImages.length > 0) {
+            testHTML = (<div className="App">
+                <LightGallery
+                    speed={500}
+                    addClass={"testing-stuff"}
+                    plugins={[lgThumbnail, lgZoom]}
+                >
+                    {
+                        allImages.map(image =>
+                            <a href={image.guid}>
+                            <img style={{width:"150px", height:"102px", "object-fit":"cover", opacity:"0.9"}} src={image.guid}></img>
+                            </a>
+                        )
+                    }
+                </LightGallery>
+            </div>)
+        }
+    }
+
 
 
     console.log(allImages);
@@ -26,11 +62,11 @@ export function Playerpictures(props) {
         if (allImages.length > 0) {
             imagesHTML = (<div>
                 {allImages.map(image =>
-                        <img src={image.guid}></img>
+                    <img src={image.guid}></img>
                 )}
             </div>)
         }
     }
 
-    return (<div>{imagesHTML}</div>)
+    return (<div>{testHTML}</div>)
 }
